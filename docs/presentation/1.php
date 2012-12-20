afec0751527d0ed5b7872a5abbb8a69230018ebe
Thibaut Marmin <marminthibaut@gmail.com>
2012-12-20 12:32:28 +0100

    ajout first commit presentation

---
+++ docs/presentation/1.php
@@ -0,0 +1,78 @@
+<div class="slide">
+	<h1>TAWEB - Flex</h1>
+	<div style="text-align:center;border:2px solid silver;margin-top:5em;background-color:#F0F0F0;border-left:none;border-right:none">
+	<h2 style="border:none;">Analyse et développement d'un effet fisheye</h2>
+	<p><span>Université Montpellier 2</span></p>
+	<p><span style="font-style:italic">Thibaut Marmin - Clément Sipieter</span></p>
+	<p>Jeudi 20 décembre 2012</p>
+	</div>
+</div>
+
+<div class="slide">
+	<h1>Plan</h1>
+		<div class="vbox"></div>
+	<div class="hbox" style="margin-top: 2em; height: auto;">
+	<ul>
+	<li><b><u>Approche globale</u></b></li>
+    <li>Approche propagée</li>
+    <li>Approche avancée</li>
+	</ul>
+	</div>
+</div>
+
+<div class="slide">
+	<h1>Approche globale</h1>
+	<p>
+	    Déterminer les dimensions et positions à partir de :
+	    <ul>
+	      <li>la position de la souris</li>
+	      <li>la position du bloc</li>
+	    </ul>
+	    </p>
+</div>
+
+<div class="slide">
+	<h1>Approche globale</h1>
+    <h2>Initialisation</h2>
+    <center>
+        <img src="img/grob_app_init.svg" alt="oops" />
+    </center>
+</div>
+
+<div class="slide">
+	<h1>Approche globale</h1>
+    <h2>Mise à jour</h2>
+    <center>
+        <img src="img/grob_app_mouse.svg" alt="oops" />
+    </center>
+</div>
+
+<div class="slide">
+	<h1>Approche globale</h1>
+    <h2>Implémentation</h2>
+    <center>
+            <object id="obj" type="application/x-shockwave-flash" data="realisations/1.swf" width="100%" height="100%">
+            <p>There are any problems</p>
+            </object>
+    </center>
+    <div class="incremental">
+   	<div class="popup spopup">
+	<ul class="positive">
+	    <li>Rapide à mettre en oeuvre</li>
+	    <li>Effet fluide</li>
+	</ul>
+	<ul class="negative">
+	    <li>Beaucoup d'espace perdu</li>
+	</ul>
+	 </div>
+	 </div>
+
+</div>
+
+<div class="slide">
+	<h1>Approche globale</h1>
+    <h2>Évolution</h2>
+    <center>
+        <img src="img/grob_app_evo.svg" alt="oops" />
+    </center>
+</div>
